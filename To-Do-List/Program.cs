3456b62ff2acc7080bc22afd50bc5347d82da132
SiriNeeradi <sirineeradi07@gmail.com>
2025-06-13 15:56:39 +0530

    To-Do-List Project with AddTask method

---
+++ To-Do-List/Program.cs
@@ -0,0 +1,40 @@
+﻿public class ToDoList
+{
+
+    public static string[] tasks = new string[10];
+    public static int taskCount = 0;
+
+   public static void AddTask()
+    {
+        Console.WriteLine("Enter a new task:");
+        tasks[taskCount] = Console.ReadLine();
+        taskCount++;
+    }
+
+    public static void Main(string[] args)
+    {
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1. Add a task");
+            Console.WriteLine("2. Exit");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    AddTask();
+                    break;
+                case "2":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+}
